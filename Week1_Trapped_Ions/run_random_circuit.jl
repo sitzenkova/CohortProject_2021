24e74963fabbb24a704ff5b3279865673f60ede0
maniashish3 <59875730+maniashish3@users.noreply.github.com>
2021-07-09 10:09:54 +0530

    Update run_random_circuit.jl
    computing |<x|ψ>^|2| <x|\psi>^ |2| with tensors theoretically

--- Week1_Trapped_Ions/run_random_circuit.jl
+++ Week1_Trapped_Ions/run_random_circuit.jl
@@ -46,7 +46,31 @@ function run(N, depth)
     psi = runcircuit(N, gates)
 end
 
-N = parse(Int, ARGS[1])
-depth = parse(Int, ARGS[2])
+#Input Parameters
+N = 4
+depth = 2
 
-run(N, depth)
+psi = run(N, depth) #Store the MPS state
+
+using ITensors
+sites = siteinds("S=1/2",N)
+states = [isodd(n) ? "Up" : "Dn" for n=1:N]
+
+A = zeros(1, 2^N) # for storing theoretical probability of each qubit bit string
+for x in 0:2^N-1
+    y = digits(x, base=2, pad=N)    #Convert Decimal Integer to Binary Number
+
+for n in 1:N
+    if y[n] == 0
+    states[n] = "Dn" # Quantum State "Dn" when corresponding bit is zero
+    else
+    states[n] = "Up" # Quantum State "Up" when corresponding bit is one
+    end
+end
+    xpsi = productMPS(sites,states) # Quantum state corresponding to Classical Bit String
+    a = inner(xpsi, psi) # Probability Amplitude of xpsi in psi
+    b = a * a'          # Probability of occurance of xpsi upon measurement
+    A[1,x+1] = b
+    
+end
+println(A)
